abc6eb323c03fd661937fe248d4494556c761575
Matt Beyl <matthew.beyl@gmail.com>
2018-08-21 17:34:09 -0500

    working in router on player post

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -13,23 +13,29 @@ router.get('/:player_type', (req, res) => {
         res.send(response.body)
     })
 });
-
 router.post('/player', (req, res) => {
-    console.log(req.body);
-    pool.query(`INSERT INTO "player"
-    ("playerId", "jersey", "displayName", "team", "position", "height", "weight", "dob", "college", "team_id")
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`, [])
-        .then((results) => {
-            res.sendStatus(201);
-        }).catch((errorFromPG) => {
-            res.sendStatus(500);
+    pool.query(`SELECT team.id FROM team
+JOIN person ON team.person_id = person.id
+WHERE person.id = $1;`, [req.user.id])
+        .then((response) => {
+            let teamId = response.rows[0].id;
         })
+
 })
-//SELECT team.id FROM person
-//JOIN person ON team.id = team.person_id = person.id
-//WHERE person.id = $1
-//INSERT INTO "player"
-//("playerId", "jersey", "displayName", "team")
+
+// router.post('/player', (req, res) => {
+//     console.log(req.user.id);
+//     pool.query(`INSERT INTO "player"
+//     ("playerId", "jersey", "displayName", "team", "position", "height", "weight", "dob", "college", "team_id")
+//     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`, [req.body.playerId, req.body.jersey, req.body.displayName,
+//     req.body.team, req.body.position, req.body.height, req.body.weight, req.body.dob, req.body.college, req.team.id])
+//         .then((results) => {
+//             res.sendStatus(201);
+//         }).catch((errorFromPG) => {
+//             res.sendStatus(500);
+//         })
+// })
+
 router.post('/team', (req, res) => {
     console.log(req.user.id);
     console.log(req.body.teamName);
